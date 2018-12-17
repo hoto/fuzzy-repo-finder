0340c5e99a21f8d58a40a12031948754a77e4741
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-12-17 14:33:04 +0000

    Start from scratch

--- main.go
+++ main.go
@@ -1,25 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-
-	flag "github.com/ogier/pflag"
-)
-
-var (
-	user string
-)
-
 func main() {
-	flag.Parse()
-	users := strings.Split(user, ",")
-	fmt.Printf("Searching users: %s\n", users)
-	result := getUsers(user)
-	fmt.Println(`Name: `, result.Name)
-	fmt.Println(`Location: `, result.Location)
-}
-
-func init() {
-	flag.StringVarP(&user, "user", "u", "", "Search Users")
 }
