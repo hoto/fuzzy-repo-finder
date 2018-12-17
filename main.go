02a0c14511740a8726cfe950999627e5e3c849bc
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-12-17 15:08:39 +0000

    Find all .git folders

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+const (
+	projectsDir = "/home/andrzej.rehmann/projects"
+)
+
 func main() {
+	err := filepath.Walk(projectsDir,
+		func(path string, file os.FileInfo, err error) error {
+			if file.IsDir() && file.Name() == ".git" {
+				if err != nil {
+					return err
+				}
+				fmt.Println(path)
+			}
+			return nil
+		})
+
+	if err != nil {
+		log.Println(err)
+	}
 }
