d18e5e5a04bcf9ac7f818d709cda5ea75ea344fa
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-12-21 20:53:11 +0000

    Add doc to terminal.Init()

--- internal/io/terminal.go
+++ internal/io/terminal.go
@@ -10,6 +10,11 @@ func NewTerminal() *Terminal {
 	return &Terminal{}
 }
 
+// Initializes terminal. This function must be called before any other functions.
+// Terminal must be closed using Close() function before exiting the application.
+//
+//      keyboard := terminal.Init()
+//      defer terminal.Close()
 func (t Terminal) Init() *Keyboard {
 	err := termbox.Init()
 	if err != nil {
