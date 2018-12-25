e20e8251c764eeca61afc829eeda5f57c1da0213
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-12-25 20:54:47 +0000

    Add Copy() and Size() to Projects

--- internal/proj/projects_test.go
+++ internal/proj/projects_test.go
@@ -9,6 +9,12 @@ var (
 	emptyProjects = make([]Project, 0)
 )
 
+func Test_should_be_empty(t *testing.T) {
+	projects := NewProjects()
+
+	assert.Equal(t, 0, projects.Size())
+}
+
 func Test_should_return_empty_projects(t *testing.T) {
 	projects := NewProjects()
 
@@ -21,6 +27,14 @@ func Test_should_retain_a_project(t *testing.T) {
 	projects.Add(project)
 
 	assert.Equal(t, projects.List(), []Project{project})
+}
+
+func Test_should_have_one_element(t *testing.T) {
+	project := Project{Name: "PROJECT_1"}
+	projects := NewProjects()
+	projects.Add(project)
+
+	assert.Equal(t, 1, projects.Size())
 }
 
 func Test_should_retain_added_projects(t *testing.T) {
@@ -53,4 +67,34 @@ func Test_should_return_groups_of_projects(t *testing.T) {
 	groups := projects.ListGroups()
 
 	assert.EqualValues(t, []string{"GROUP_1", "GROUP_2"}, groups)
+}
+
+func Test_should_make_an_empty_copy(t *testing.T) {
+	projects := NewProjects()
+
+	projectsCopy := projects.Copy()
+
+	assert.Equal(t, projectsCopy, projects)
+}
+
+func Test_should_make_a_copy(t *testing.T) {
+	projects := NewProjects()
+	project1 := Project{Name: "PROJECT_1", Group: "GROUP_1"}
+	projects.AddAll([]Project{project1})
+
+	projectsCopy := projects.Copy()
+
+	assert.Equal(t, projectsCopy, projects)
+}
+
+func Test_should_make_a_deep_copy(t *testing.T) {
+	projects := NewProjects()
+	project1 := Project{Name: "PROJECT_1", Group: "GROUP_1"}
+	project2 := Project{Name: "PROJECT_2", Group: "GROUP_2"}
+	projects.AddAll([]Project{project1})
+
+	projectsCopy := projects.Copy()
+	projects.Add(project2)
+
+	assert.NotEqual(t, projectsCopy, projects)
 }
