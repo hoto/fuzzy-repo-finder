9b5cbb32f59e2ed8ed9b321d810ee1c22f1b3d43
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-03-10 18:19:19 +0000

    Log hardcoded version when --version flag is passed

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,10 +4,12 @@ import (
 	"flag"
 	"fmt"
 	. "github.com/logrusorgru/aurora"
+	"os"
 	"strings"
 )
 
 var (
+	Version             string
 	Debug               bool
 	ProjectsRoots       []string
 	ProjectNameFilter   string
@@ -15,26 +17,32 @@ var (
 )
 
 func ParseArgsAndFlags() {
-
 	flag.BoolVar(&Debug, "debug", false, "Show verbose debug information")
-	//version := flag.Bool("version", false, "Show version")
+	version := flag.Bool("version", false, "Show version")
 	projectRoots := flag.String("projectRoots", "/default-projects-path", "Comma separated list of project roots directories")
 	flag.Parse()
 
 	ProjectNameFilter = strings.Join(flag.Args(), "")
 	ProjectsRoots = strings.Split(*projectRoots, ",")
 
-	if Debug {
-		fmt.Println("Flags:")
-		fmt.Printf("  projectRoots=%s\n", Cyan(*projectRoots))
-
-		fmt.Println()
-		fmt.Println("Args:")
-		fmt.Printf("  args=%s\n", Cyan(flag.Args()))
-
-		fmt.Println()
-		fmt.Println("Config:")
-		fmt.Printf("  ProjectRoots=%s\n", Cyan(ProjectsRoots))
-		fmt.Printf("  ProjectNameFilter=%s\n", Cyan(ProjectNameFilter))
+	if *version {
+		fmt.Printf("version=%s\n", Version)
+		os.Exit(0)
 	}
+
+	if Debug {
+		debugLog(projectRoots)
+	}
+}
+
+func debugLog(projectRoots *string) {
+	fmt.Println("Flags:")
+	fmt.Printf("  projectRoots=%s\n", Cyan(*projectRoots))
+	fmt.Println()
+	fmt.Println("Args:")
+	fmt.Printf("  args=%s\n", Cyan(flag.Args()))
+	fmt.Println()
+	fmt.Println("Config:")
+	fmt.Printf("  ProjectRoots=%s\n", Cyan(ProjectsRoots))
+	fmt.Printf("  ProjectNameFilter=%s\n", Cyan(ProjectNameFilter))
 }
