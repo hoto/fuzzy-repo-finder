06c95af2198cab7fe3eb0515a9e40b103d7308c8
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-11 19:02:32 +0100

    Get rid of useless stuff

--- cmd/fuzzy-repo-finder/test.go
+++ cmd/fuzzy-repo-finder/test.go
@@ -7,23 +7,21 @@ import (
 )
 
 func main() {
-	level := 0
 	//dir := "/home/andrzej.rehmann/projects"
 	dir := "/home/andrzej.rehmann/go"
-	scan(level, dir, "")
+	scan(dir)
 }
 
-func scan(level int, dir string, folderName string) {
+func scan(dir string) {
 	files, err := ioutil.ReadDir(dir)
 	check(err)
 	if isAGitRepo(files) {
 		return
 	}
-	for i, file := range files {
-		fmt.Printf("%d-%d %s/%s\n", level, i, dir, file.Name())
+	for _, file := range files {
+		fmt.Printf("%s/%s\n", dir, file.Name())
 		if file.IsDir() {
-			nextLevel := level + 1
-			scan(nextLevel, fmt.Sprintf("%s/%s", dir, file.Name()), file.Name())
+			scan(fmt.Sprintf("%s/%s", dir, file.Name()))
 		}
 	}
 }
