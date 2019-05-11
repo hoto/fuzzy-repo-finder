45b858caaf71e8514a7ae1d95b8f1b7aecb28ed9
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2019-05-11 19:20:23 +0100

    Append matching paths to a slice

--- cmd/fuzzy-repo-finder/test.go
+++ cmd/fuzzy-repo-finder/test.go
@@ -7,22 +7,26 @@ import (
 )
 
 func main() {
-	//dir := "/home/andrzej.rehmann/projects"
-	root := "/home/andrzej.rehmann/go"
-	scan(root, ".git")
+	//root := "/home/andrzej.rehmann/projects"
+	root := "/home/andrzej.rehmann/go/src"
+	matchingPaths := make([]string, 0)
+	scan(root, ".git", &matchingPaths)
+	for i, e := range matchingPaths {
+		fmt.Printf("%d %s\n", i, e)
+	}
 }
 
-func scan(root string, needle string) {
+func scan(root string, needle string, matchingPaths *[]string) {
 	haystack, err := ioutil.ReadDir(root)
 	check(err)
 	if containsNeedle(haystack, needle) {
+		*matchingPaths = append(*matchingPaths, root)
 		return
 	}
 	for _, file := range haystack {
-		fmt.Printf("%s/%s\n", root, file.Name())
 		if file.IsDir() {
 			dir := fmt.Sprintf("%s/%s", root, file.Name())
-			scan(dir, needle)
+			scan(dir, needle, matchingPaths)
 		}
 	}
 }
